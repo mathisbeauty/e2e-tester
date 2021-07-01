216858884a5581771ff5e724e9b1459e634384b1
Tomás Dornell <tomascoding@gmail.com>
2021-07-01 14:12:31 +0200

    Install playwright

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-import GanacheChain from './chains/eth';
+// import GanacheChain from './chains/eth';
 
 const startTests = async () => {
-  // Create adapter
-  const chainAdapter = new GanacheChain();
-  // Set DB path to create/load a checkpoint for the chain
-  // Start chain process
-  chainAdapter.start();
-
-  // Get chain process instance
-  const chainInstance = chainAdapter.getInstance();
-
-  setTimeout(() => {
-    chainAdapter.stop();
-    // process.exit(0);
-  }, 3000);
+  // // Create adapter
+  // const chainAdapter = new GanacheChain();
+  // // Set DB path to create/load a checkpoint for the chain
+  // // Start chain process
+  // chainAdapter.start();
+  // // Get chain process instance
+  // const chainInstance = chainAdapter.getInstance();
+  // setTimeout(() => {
+  //   chainAdapter.stop();
+  //   // process.exit(0);
+  // }, 3000);
 };
 
-startTests();
+import { chromium } from 'playwright';
+
+(async () => {
+  // Make sure to run headed.
+  const browser = await chromium.launch({ headless: false });
+
+  // Setup context however you like.
+  const context = await browser.newContext({
+    /* pass any options */
+  });
+  await context.route('**/*', (route) => route.continue());
+
+  // Pause the page, and start recording manually.
+  const page = await context.newPage();
+  await page.pause();
+})();
+
+// startTests();
